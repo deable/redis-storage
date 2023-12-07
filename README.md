4027c71352e68819215df294f978c5e1293d20af
Lukas Povoda <lupo112@gmail.com>
2023-12-07 19:25:20 +0100

    basic functionality

---
+++ README.md
@@ -0,0 +1,24 @@
+deable / redis-storage
+======================
+
+Simple redis cache implementation for nette framework.
+Thanks to this library, you can simplify your workflow with the redis as a cache storage and [Nette Framework](https://nette.org/).
+
+Requirements
+------------
+
+This library was developed for PHP 7.4 or newer, designed for [Nette Framework](https://nette.org/) version 3.1 or newer.
+
+Installation
+------------
+
+The best way to install this library is using [Composer](https://getcomposer.org/):
+
+```sh
+$ composer require deable/redis-storage
+```
+
+Contributing
+------------
+This is an open source, community-driven project. If you would like to contribute,
+please follow the code format as used in current sources and submit a pull request.
---
+++ src/RedisStorage.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use Deable\Redis\RedisClient;
+use Deable\RedisCache\RedisCache;
+use Nette\Caching\Storage;
+
+/**
+ * Class RedisStorage
+ *
+ * @package Deable\RedisStorage
+ */
+final class RedisStorage implements Storage
+{
+	private RedisClient $client;
+
+	private RedisCache $cache;
+
+	public function __construct(RedisClient $client)
+	{
+		$this->client = $client;
+		$this->cache = new RedisCache($client);
+	}
+
+	public function read(string $key)
+	{
+		return $this->cache->get($key);
+	}
+
+	public function lock(string $key): void
+	{
+		if (!$this->client->getLock()->acquire($key)) {
+			throw new RuntimeException("Cannot lock cache key '$key'.");
+		}
+	}
+
+	public function write(string $key, $data, array $dependencies): void
+	{
+		$this->cache->set($key, $data);
+		$this->client->getLock()->release($key);
+	}
+
+	public function remove(string $key): void
+	{
+		$this->cache->delete($key);
+	}
+
+	public function clean(array $conditions): void
+	{
+		$this->cache->clear();
+	}
+
+}
